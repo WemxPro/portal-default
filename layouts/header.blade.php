e7e06c19b11d325ed18fbe1a148c4f41e68b77e2
PadowYT2 <me@padow.ru>
2024-06-26 22:14:37 +0300

    Update header.blade.php

--- layouts/header.blade.php
+++ layouts/header.blade.php
@@ -52,7 +52,8 @@
                         @if (in_array('navbar', $page->placement))
                             <li class="mr-2">
                                 <a class="{{ is_active('page', ['page' => $page->path]) }} group inline-flex rounded-t-lg border-b-2 border-gray-50 px-4 py-4 text-center text-sm font-medium text-gray-500 dark:border-gray-800 dark:text-gray-400"
-                                    href="{{ route('page', $page->path) }}" @if ($page->new_tab) target="_blank" @endif>
+                                    href="{{ route('page', $page->path) }}"
+                                    @if ($page->new_tab) target="_blank" @endif>
                                     <span class="mr-2" style="font-size: 20px;">
                                         {!! $page->icon !!}
                                     </span>
@@ -86,63 +87,33 @@
 @endauth
 
 @guest
-<<<<<<< Updated upstream
     <header>
-        <nav class="border-gray-200 bg-white px-4 py-2.5 dark:bg-gray-800 lg:px-6">
-            <div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
+        <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+            <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <a href="/" class="flex items-center">
-                    <img src="@settings('logo', '/assets/core/img/logo.png')" class="mr-3 h-6 sm:h-9" alt="@settings('app_name', 'WemX')" />
-                    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">@settings('app_name', 'WemX')</span>
+                    <img src="@settings('logo', '/assets/core/img/logo.png')" class="mr-3 h-6 sm:h-9"
+                        alt="@settings('app_name', 'WemX')" />
+                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">@settings('app_name',
+                        'WemX')</span>
                 </a>
-
                 @include(Theme::path('layouts.widgets.user-dropdown'))
-
-                <div class="hidden w-full items-center justify-between lg:order-1 lg:flex lg:w-auto" id="mobile-menu-2">
-                    <ul class="mt-4 flex flex-col font-medium lg:mt-0 lg:flex-row lg:space-x-8">
+                <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                    <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                         <li>
                             <a href="#home"
-                                class="bg-primary-700 lg:text-primary-700 block rounded py-2 pl-3 pr-4 text-white dark:text-white lg:bg-transparent lg:p-0"
-                                aria-current="page">
-                                {!! __('client.home') !!}
-                            </a>
+                                class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                                aria-current="page">{!! __('client.home') !!}</a>
                         </li>
                         <li>
                             <a href="#features"
-                                class="lg:hover:text-primary-700 block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:border-0 lg:p-0 lg:hover:bg-transparent lg:dark:hover:bg-transparent lg:dark:hover:text-white">
-                                {!! __('client.features') !!}
-                            </a>
+                                class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">{!! __('client.features') !!}</a>
                         </li>
                         <li>
                             <a href="#pricing"
-                                class="lg:hover:text-primary-700 block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:border-0 lg:p-0 lg:hover:bg-transparent lg:dark:hover:bg-transparent lg:dark:hover:text-white">
-                                {!! __('client.pricing') !!}
-                            </a>
+                                class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">{!! __('client.pricing') !!}</a>
                         </li>
                     </ul>
                 </div>
-=======
-<header>
-    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="/" class="flex items-center">
-                <img src="@settings('logo', '/assets/core/img/logo.png')" class="mr-3 h-6 sm:h-9" alt="@settings('app_name', 'WemX')" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">@settings('app_name', 'WemX')</span>
-            </a>
-            @include(Theme::path('layouts.widgets.user-dropdown'))
-            <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                    <li>
-                        <a href="#home" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">{!! __('client.home') !!}</a>
-                    </li>
-                    <li>
-                        <a href="#features" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">{!! __('client.features') !!}</a>
-                    </li>
-                    <li>
-                        <a href="#pricing" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">{!! __('client.pricing') !!}</a>
-                    </li>
-                </ul>
->>>>>>> Stashed changes
-            </div>
         </nav>
     </header>
 @endguest
